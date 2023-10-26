2849d898564fe1e61fa02dbfece36c53daecd516
Jackson <jacksonbnetherwoodimig@gmail.com>
2023-10-26 15:06:41 -0700

    fix drivebase

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import (Port, Stop, Direction, Button, Color,
-                                 SoundFile, ImageFile, Align)
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
 
 import struct
 
@@ -11,20 +10,40 @@ import struct
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.B)
 arm_motor = Motor(Port.C)
+
+# Defining variables that will store the dc values for the motors
 drive = 0
 steer = 0
 arm = 0
 
+# Arm speed is a value from 0-1 that is multiplied by the input value for the arm movement.
+# 1 is default, but you can lower it for better control
+arm_speed = 1
+
+#############
+# Drivebase #
+#############
+
 # A drivebase allows for precise and easy control of the robot while acting autonomously
 # Read drivebase documentation linked in the README on github to correctly set the last value
-robot = DriveBase(left_motor, right_motor, 55, 200)
+robot = DriveBase(left_motor, right_motor, 55.5, 121.5)
 
-# Auto
-'''
-example:
-robot.straight(500) # drive 500mm foreward
+# Drivebase settings
+# Arguments: straight speed: mm/s, straight acceleration: mm/s^2, turn rate: deg/s, turn acceleration: deg/s^2
+robot.settings(500, 1000, 100, 100)
+
+
+########
+# Auto #
+########
+
+# Example
+robot.straight(2000) # drive 2000mm foreward
 robot.stop()
-'''
+
+#########
+# Setup #
+#########
 
 # A helper function for converting stick values (0 - 255)
 # to more usable numbers (-100 - 100)
@@ -45,6 +64,10 @@ in_file = open(infile_path, "rb")
 FORMAT = 'llHHI'    
 EVENT_SIZE = struct.calcsize(FORMAT)
 event = in_file.read(EVENT_SIZE)
+
+#############
+# Main loop #
+#############
 
 while event:
     (tv_sec, tv_usec, ev_type, code, value) = struct.unpack(FORMAT, event)
@@ -70,7 +93,7 @@ while event:
     # Set motor voltages. 
     left_motor.dc(drive - steer)
     right_motor.dc(drive + steer)
-    arm_motor.dc(arm)
+    arm_motor.dc(arm * arm_speed)
 
     # Finally, read another event
     event = in_file.read(EVENT_SIZE)
