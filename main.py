d5b3459d9014e3a6f0e368a8e1779ac18df48397
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2024-11-13 13:21:09 -0800

    added instructions for editing main

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env pybricks-micropython
+from auto import (auto, auto_button, stop_button, disable_stop_button, left_motor, right_motor, arm_motor)
+from definitions import (ButtonEvent, AxisCode, EventType)
+
 from pybricks.ev3devices import Motor
 from pybricks.parameters import (Port, Stop, Direction, Button)
 from pybricks.tools import wait
-
-from auto import (auto, auto_button, stop_button, disable_stop_button, left_motor, right_motor, arm_motor)
-from definitions import (ButtonEvent, AxisCode, EventType)
-import struct
 import usys
-import sys
+
+import struct
+from sys import exit
+from io import BufferedReader
 
 ######################################################
 # DO NOT MODIFY THIS FILE (main.py)                  #
@@ -19,58 +21,77 @@ import sys
 ######################################################
 
 # Helper function to scale values from a source range to a destination range
-def scale(val, source, target):
+def scale(val, source: tuple[int, int], target: tuple[int, int]):
     return (float(val - source[0]) / (source[1] - source[0])) * (target[1] - target[0]) + target[0]
 
+
+def arcade_drive(ev_type: int, ev_code: int, ev_value: int):
+    drive_speed = 0
+    turn_rate = 0
+    if ev_type == EventType.AXIS:
+        if ev_code == AxisCode.LEFT_STICK_Y:
+            drive_speed = scale(ev_value, (0,255), (100, -100))
+        if ev_code == AxisCode.RIGHT_STICK_X:
+            turn_rate = scale(ev_value, (0,255), (100, -100))
+
+        left_motor.dc(drive_speed - turn_rate)
+        right_motor.dc(drive_speed + turn_rate)
+
+def tank_drive(ev_type: int, ev_code: int, ev_value: int):
+    left_power = 0
+    right_power = 0
+    if ev_type == EventType.AXIS:
+        if ev_code == AxisCode.LEFT_STICK_Y:
+            left_power = scale(ev_value, (0, 255), (100, -100))
+        if ev_code == AxisCode.RIGHT_STICK_Y:
+            right_power = scale(ev_value, (0, 255), (100, -100))
+
+    left_motor.dc(left_power)
+    right_motor.dc(right_power)
+
+def open_input_file(path: str) -> BufferedReader:
+    try:
+        in_file: BufferedReader = open(path, "rb")
+        print("Controller connected!")
+        return in_file
+    except:
+        print(f"Failed to open {path} for reading.")
+        print("Make sure your controller is turned on and connected.")
+        exit(1)
 
 # main function
 def main():
     print("Implementation: " + str(usys.implementation))
     print("Version: " + str(usys.version))
 
-
-    DATA_FORMAT = 'llHHI'    
-    EVENT_SIZE = struct.calcsize(DATA_FORMAT)
+     # Corresponds to 'sec: long, usec: long, type: ushort, code: ushort, value: uint'
+    DATA_FORMAT: str = "llHHI"
+    EVENT_SIZE: int = struct.calcsize(DATA_FORMAT)
 
     # Open controller input file
-    in_file = None
-    try:
-        in_file = open("/dev/input/event4", "rb")
-        print("Controller connected!")
-    except:
-        print("Failed to open /dev/input/event4 for reading.")
-        print("Make sure your controller is turned on and connected.")
-        exit(1)
+    in_file: BufferedReader = open_input_file("/dev/input/event4")
 
-    # Declare variables to store movement speeds as set by the controller
-    drive_speed = 0
-    turn_rate = 0
-    arm_power = 0
-
-    event = in_file.read(EVENT_SIZE)
+    event: bytes = in_file.read(EVENT_SIZE)
 
     while event:
-        (_, _, ev_type, code, value) = struct.unpack(DATA_FORMAT, event)
-        
-        # Read analog stick values    
-        if ev_type == EventType.AXIS:
-            if code == AxisCode.LEFT_STICK_Y:
-                drive_speed = scale(value, (0,255), (100,-100))
-            if code == AxisCode.RIGHT_STICK_X:
-                turn_rate = scale(value, (0,255), (100, -100))
-            if code == AxisCode.LEFT_TRIGGER:
-                arm_power = value / 3
-            if code == AxisCode.RIGHT_TRIGGER:
-                arm_power = -value / 3
+        (_, _, ev_type, ev_code, ev_value) = struct.unpack(DATA_FORMAT, event)
         if ev_type == EventType.BUTTON:
-            if code == stop_button and value == ButtonEvent.PRESSED and not disable_stop_button:
+            if ev_code == stop_button and ev_value == ButtonEvent.PRESSED and not disable_stop_button:
                 break
-            if code == auto_button and value == ButtonEvent.PRESSED:
+            if ev_code == auto_button and ev_value == ButtonEvent.PRESSED:
                 auto()
-                
+        
+        arcade_drive(ev_type, ev_code, ev_value)
+        # tank_drive(ev_type, ev_code, ev_value)
+
+        arm_power = 0
+        arm_sensitivity = 0.3
+        if ev_type == EventType.AXIS:
+            if ev_code == AxisCode.LEFT_TRIGGER:
+                arm_power = ev_value * arm_sensitivity
+            if ev_code == AxisCode.RIGHT_TRIGGER:
+                arm_power = -ev_value * arm_sensitivity
         arm_motor.dc(arm_power)
-        left_motor.dc(drive_speed - turn_rate)
-        right_motor.dc(drive_speed + turn_rate)
 
         event = in_file.read(EVENT_SIZE)
 
