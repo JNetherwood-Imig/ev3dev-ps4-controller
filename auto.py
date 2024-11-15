7471c6f0561c9d50474508f1bd85e7086c5a7145
JNetherwood-Imig <jacksonbnetherwoodimig@gmail.com>
2024-11-15 13:32:20 -0800

    fixed type hints, formatted printing, and driving logic

--- auto.py
+++ auto.py
@@ -1,22 +1,24 @@
 from definitions import ButtonCode
+
 from pybricks.robotics import DriveBase
 from pybricks.ev3devices import Motor
 from pybricks.parameters import (Port, Stop)
 
 # Declare motors 
 # DO NOT CHANGE THE VARIABLE NAMES, only the ports, such that they match your robot configuration
-left_motor  = Motor(Port.A)
-right_motor = Motor(Port.B)
-arm_motor   = Motor(Port.C)
+left_motor: Motor  = Motor(Port.A)
+right_motor: Motor = Motor(Port.B)
+arm_motor: Motor   = Motor(Port.C)
 
 # Declare which buttons to use to start auto code and stop the robot
 # A full list of button codes can be found in definitions.py
-auto_button = ButtonCode.CIRCLE
-stop_button = ButtonCode.SQUARE
-disable_stop_button = False # For competition, you may want to disable the stop button to prevent accidentally terminating your code
+auto_button: int = ButtonCode.CIRCLE
+stop_button: int = ButtonCode.SQUARE
+disable_stop_button: bool = False # For competition, you may want to disable the stop button to prevent accidentally terminating your code
+reverse_motor_direction: bool = False
 
 # Define drivebase
-drivebase = DriveBase(
+drivebase: DriveBase = DriveBase(
 	left_motor,
 	right_motor,
 	wheel_diameter=55,
@@ -25,7 +27,7 @@ drivebase = DriveBase(
 # This function will be run when you press the auto button as defined above
 # There is little limitation on what this function can do, so if you want, you could make it use a distance sensor
 # You are not limited to driving instructions
-def auto():
+def auto() -> None:
 	drivebase.straight(500) # Drives for 300mm
 	arm_motor.run_angle(500, 90, then=Stop.BRAKE) # Turn the arm motor 90 degrees cw
 	drivebase.straight(-500) # Drive backward 300mm
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pybricks-micropython
-from auto import (auto, auto_button, stop_button, disable_stop_button, left_motor, right_motor, arm_motor)
+from auto import (auto, auto_button, stop_button, disable_stop_button, left_motor, right_motor, arm_motor, reverse_motor_direction)
 from definitions import (ButtonEvent, AxisCode, EventType)
 
 from pybricks.ev3devices import Motor
@@ -9,7 +9,7 @@ import usys
 
 import struct
 from sys import exit
-from io import BufferedReader
+import io
 
 ######################################################
 # DO NOT MODIFY THIS FILE (main.py)                  #
@@ -21,46 +21,60 @@ from io import BufferedReader
 ######################################################
 
 # Helper function to scale values from a source range to a destination range
-def scale(val, source: tuple[int, int], target: tuple[int, int]):
+def scale(val: float, source: tuple[int, int], target: tuple[int, int]) -> float:
     return (float(val - source[0]) / (source[1] - source[0])) * (target[1] - target[0]) + target[0]
 
 
-def arcade_drive(ev_type: int, ev_code: int, ev_value: int):
-    drive_speed = 0
-    turn_rate = 0
+drive_speed: float = 0
+turn_rate: float = 0
+def arcade_drive(ev_type: int, ev_code: int, ev_value: int) -> None:
+    global drive_speed
+    global turn_rate
     if ev_type == EventType.AXIS:
         if ev_code == AxisCode.LEFT_STICK_Y:
             drive_speed = scale(ev_value, (0,255), (100, -100))
         if ev_code == AxisCode.RIGHT_STICK_X:
             turn_rate = scale(ev_value, (0,255), (100, -100))
+    
+    left_power: float = drive_speed - turn_rate
+    right_power: float = drive_speed + turn_rate
 
-        left_motor.dc(drive_speed - turn_rate)
-        right_motor.dc(drive_speed + turn_rate)
+    if reverse_motor_direction:
+        left_power *= -1
+        right_power += -1
 
-def tank_drive(ev_type: int, ev_code: int, ev_value: int):
-    left_power = 0
-    right_power = 0
+    left_motor.dc(left_power)
+    right_motor.dc(right_power)
+
+left_power: float = 0
+right_power: float = 0
+def tank_drive(ev_type: int, ev_code: int, ev_value: int) -> None:
+    global left_power
+    global right_power
     if ev_type == EventType.AXIS:
         if ev_code == AxisCode.LEFT_STICK_Y:
             left_power = scale(ev_value, (0, 255), (100, -100))
         if ev_code == AxisCode.RIGHT_STICK_Y:
             right_power = scale(ev_value, (0, 255), (100, -100))
+    if reverse_motor_direction:
+        left_power *= -1
+        right_power += -1
 
     left_motor.dc(left_power)
     right_motor.dc(right_power)
 
-def open_input_file(path: str) -> BufferedReader:
+def open_input_file(path: str) -> io.BufferedReader:
     try:
-        in_file: BufferedReader = open(path, "rb")
+        in_file: io.BufferedReader = open(path, "rb")
         print("Controller connected!")
         return in_file
     except:
-        print(f"Failed to open {path} for reading.")
+        print("Failed to open {} for reading.".format(path))
         print("Make sure your controller is turned on and connected.")
         exit(1)
 
 # main function
-def main():
+def main() -> None:
     print("Implementation: " + str(usys.implementation))
     print("Version: " + str(usys.version))
 
@@ -69,7 +83,7 @@ def main():
     EVENT_SIZE: int = struct.calcsize(DATA_FORMAT)
 
     # Open controller input file
-    in_file: BufferedReader = open_input_file("/dev/input/event4")
+    in_file: io.BufferedReader = open_input_file("/dev/input/event4")
 
     event: bytes = in_file.read(EVENT_SIZE)
 
